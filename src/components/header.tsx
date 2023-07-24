6af8c33a25fc023a16023515200b46833adeda26
filimda <filimda@yandex.ru>
2023-07-24 22:59:35 +0300

    modified:   src/app/page.tsx 	new file:   src/components/header.tsx

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+
+    export default async function Header() {
+    return (
+
+        <div className="bg-gray-50 dark:bg-gray-800">
+            Дима молодец
+        </div>
+    );
+        
+    
+}
